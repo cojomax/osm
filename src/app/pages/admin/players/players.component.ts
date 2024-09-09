eba71de8140fd58aea2aa6a25b184412ec5037de
cojomax <maxcuentas@pm.me>
2024-09-09 19:54:36 +0800

    add ag grid

--- src/app/pages/admin/players/players.component.ts
+++ src/app/pages/admin/players/players.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { PlayerFormComponent } from "../../../components/forms/player/player.form";
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { tap } from 'rxjs';
+import { Player } from 'src/app/domain/player/player.model';
+import { PlayerService } from 'src/app/domain/player/player.service';
+import { PlayerFormComponent } from '../../../components/forms/player/player.form';
 
 @Component({
   standalone: true,
-  imports: [PlayerFormComponent],
+  imports: [AgGridAngular, PlayerFormComponent],
   templateUrl: './players.component.html',
-  styleUrl: './players.component.css'
+  styleUrl: './players.component.css',
 })
 export class PlayersPageComponent {
+  protected players: Player[] = [];
 
+  constructor(private _playerSvc: PlayerService) {}
+
+  ngOnInit() {
+    this._playerSvc
+      .getAllPlayers()
+      .pipe(
+        tap((players) => {
+          this.players = players;
+        }),
+      )
+      .subscribe();
+  }
+
+  protected colDefs: ColDef[] = [
+    { field: 'squadNumber', headerName: '#' },
+    { field: 'firstName', editable: true },
+    { field: 'lastName' },
+    { field: 'position' },
+  ];
 }
