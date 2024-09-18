521797dd060470a53f3430503124bbdc94d309c4
cojomax <maxcuentas@pm.me>
2024-09-19 07:20:57 +0800

    add module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from '@nz/layout';
 import { AdminNavComponent } from './components/admin/nav/admin-nav.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
@@ -8,7 +8,7 @@ import { FirebaseService } from './services/firebase/firebase.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AdminNavComponent, RouterOutlet, NavigationComponent, NzLayoutModule],
+  imports: [AdminNavComponent, RouterModule, RouterOutlet, NavigationComponent, NzLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
