e826c7ee8a9541d391413444318da6e514551964
cojomax <maxcuentas@pm.me>
2024-09-23 21:58:57 +0800

    disable routes temp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,14 @@ import { PlayerDetailsPageComponent } from './pages/admin/players/details/player
 import { PlayersPageComponent } from './pages/admin/players/players.component';
 import { HomePageComponent } from './pages/home/home.page';
 import { LoginPageComponent } from './pages/login/login.page';
-import { MatchReportPageComponent } from './pages/match-report/match-report.page';
 import { PageNotFoundPageComponent } from './pages/not-found/not-found.page';
-import { RegisterPageComponent } from './pages/register/register.page';
 import { TeamPageComponent } from './pages/team/team.page';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'match-report', component: MatchReportPageComponent },
+  // { path: 'register', component: RegisterPageComponent },
+  // { path: 'match-report', component: MatchReportPageComponent },
   { path: 'team', component: TeamPageComponent },
   {
     path: 'admin',
