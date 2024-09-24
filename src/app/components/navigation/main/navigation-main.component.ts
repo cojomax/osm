45ce3eb2c05b9031b1b14aceb1c6e162222d282c
cojomax <maxcuentas@pm.me>
2024-09-24 08:39:20 +0800

    style main nav

--- src/app/components/navigation/main/navigation-main.component.ts
+++ src/app/components/navigation/main/navigation-main.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NzMenuModule } from '@nz/menu';
 
 @Component({
   selector: 'app-navigation-main',
   templateUrl: './navigation-main.component.html',
   styleUrl: './navigation-main.component.css',
   standalone: true,
-  imports: [NzMenuModule, RouterModule],
+  imports: [RouterModule],
 })
 export class NavigationMainComponent {}
