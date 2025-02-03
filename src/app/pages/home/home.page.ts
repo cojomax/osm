0202de41171c3a0ffc2d2ac4427862a8c49471e6
Max Cobbett <maxcuentas@pm.me>
2025-02-03 21:46:11 +0800

    add banner format next match

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: 'home.page.html'
+  templateUrl: 'home.page.html',
+  styleUrl: './home.page.css',
 })
 export class HomePageComponent implements OnInit {
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() { }
+  ngOnInit() {}
 }
