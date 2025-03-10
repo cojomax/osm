aeca919156436bc6d55a857029f83ce21be693bf
Max Cobbett <maxcuentas@pm.me>
2025-03-11 07:20:26 +0800

    read next fixture

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FixtureService } from '../../services/fixture.service';
+import { Fixture } from '../../api/models/fixture.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   templateUrl: 'home.page.html',
   styleUrl: './home.page.css',
+  imports: [DatePipe],
 })
 export class HomePageComponent implements OnInit {
-  constructor() {}
+  protected nextFixture = signal<Fixture | null>(null);
 
-  ngOnInit() {}
+  constructor(private fixtureSvc: FixtureService) {}
+
+  ngOnInit() {
+    this.fixtureSvc.fetch().subscribe((data) => {
+      // Find the fixture with the date in the future that's closes to today's date.
+      const nextFixture = data
+        .filter((fixture) => (fixture.date ?? -1) > new Date())
+        .reduce((closest, current) => ((current?.date ?? -1) < (closest?.date ?? -1) ? current : closest));
+
+      this.nextFixture.set(nextFixture);
+      console.log(data);
+    });
+  }
 }
