30973415197438269dc49f3e91171375aef2ee5c
Max Cobbett <maxcuentas@pm.me>
2025-03-15 21:29:03 +0800

    clean up admin shell

--- src/app/components/admin/shell/admin-shell.component.ts
+++ src/app/components/admin/shell/admin-shell.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 import { NavigationAdminComponent } from '../../navigation/admin/navigation-admin.component';
 
 @Component({
-    selector: 'app-admin-shell',
-    imports: [NavigationAdminComponent],
-    templateUrl: './admin-shell.component.html',
-    styleUrl: './admin-shell.component.css'
+  selector: 'app-admin-shell',
+  imports: [NavigationAdminComponent],
+  styles: `
+    .nav-container {
+      padding: 0 var(--space-md);
+    }
+
+    .content-container {
+      height: 100%;
+      padding: 0 var(--space-lg);
+    }
+  `,
+  template: `
+    <div class="nav-container">
+      <app-navigation-admin></app-navigation-admin>
+    </div>
+    <div class="content-container">
+      <ng-content></ng-content>
+    </div>
+  `,
 })
 export class AdminShellComponent {}
