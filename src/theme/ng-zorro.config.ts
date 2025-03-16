017952a80989bb06345b419c20fb6d13ce0a21cb
Max Cobbett <maxcuentas@pm.me>
2025-03-16 12:07:44 +0800

    configure theme colors

--- src/theme/ng-zorro.config.ts
+++ src/theme/ng-zorro.config.ts
@@ -2,8 +2,7 @@ import { NzConfig } from 'ng-zorro-antd/core/config';
 
 export const ngZorroConfig: NzConfig = {
   theme: {
-    // primaryColor: '#1d1e2c',
-    primaryColor: '#ddb967',
-    // secondaryColor: '#a52a2a'
+    primaryColor: '#1d1e2c',
+    secondaryColor: '#ddb967',
   },
 };
