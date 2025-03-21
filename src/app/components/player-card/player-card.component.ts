bb849fed74954cfa52fd31b2e29031a633cad8d2
Max Cobbett <maxcuentas@pm.me>
2025-03-22 00:48:03 +0800

    update card

--- src/app/components/player-card/player-card.component.ts
+++ src/app/components/player-card/player-card.component.ts
@@ -1,8 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { NzCardModule } from '@nz/card';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { Player } from '../../api/models/player.model';
+
+export enum Statistic {
+  Goals = 'Goals',
+  Assists = 'Assists',
+  CleanSheets = 'Clean Sheets',
+}
+
+type Stat = {
+  type: Statistic;
+  value: number;
+};
 
 @Component({
   selector: 'osm-player-card',
@@ -12,4 +23,12 @@ import { Player } from '../../api/models/player.model';
 })
 export class PlayerCardComponent {
   player = input<Player | undefined>();
+  stats = input<Stat[] | undefined>();
+
+  protected bestStats = computed(() => {
+    return this.stats()
+      ?.sort((a, b) => b.value - a.value)
+      .filter((_, i) => i <= 1)
+      .map((stat) => ({ label: stat.type, value: stat.value }));
+  });
 }
