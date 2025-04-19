ea0dd0ab90068ad00626f821ae7a0a5917eaef17
Max Cobbett <maxcuentas@pm.me>
2025-04-19 13:43:17 +0800

    update swiper

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -1,8 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, effect, inject, input, Signal, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, input } from '@angular/core';
 import { PlayerCardComponent } from '../player-card/player-card.component';
 import { CommonModule } from '@angular/common';
-import { IS_MOBILE } from '../../services/tokens/device-detection.token';
 import { PlayerStatistic } from '../../models/player-statistic.model';
+import { IS_MOBILE } from '../../services/tokens/is-mobile.token';
 
 @Component({
   selector: 'osm-swiper',
@@ -14,16 +14,5 @@ import { PlayerStatistic } from '../../models/player-statistic.model';
 export class SwiperComponent {
   data = input<PlayerStatistic[]>([]);
 
-  protected slidesPerView = signal<'auto' | 3>(3);
-  protected centeredSlides = signal(false);
-  protected isMobile: Signal<boolean>;
-
-  constructor() {
-    effect(() => {
-      this.slidesPerView.set(this.isMobile() ? 'auto' : 3);
-      this.centeredSlides.set(this.isMobile());
-    });
-
-    this.isMobile = signal(inject(IS_MOBILE)).asReadonly();
-  }
+  protected isMobile = inject(IS_MOBILE);
 }
