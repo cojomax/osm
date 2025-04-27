8a3455623987a797f2310a9e11776b930888325a
Max Cobbett <maxcuentas@pm.me>
2025-04-27 13:04:16 +0800

    add environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+      apiKey: 'AIzaSyAMLE6A1ZnV7_a3ihenc-CTXQRdXqMjTao',
+      authDomain: 'old-speckled-men.firebaseapp.com',
+      projectId: 'old-speckled-men',
+      storageBucket: 'old-speckled-men.firebasestorage.app',
+      messagingSenderId: '181570813115',
+      appId: '1:181570813115:web:1e5306890828851d7b3d9e',
+    },
+  };
+  
