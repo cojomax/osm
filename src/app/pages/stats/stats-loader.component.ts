33a4c48388226fe10d2e8617a2e3c12a955d6abc
Max Cobbett <maxcuentas@pm.me>
2025-05-30 14:20:50 +0800

    add stats loader

---
+++ src/app/pages/stats/stats-loader.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgComponentOutlet } from '@angular/common';
+import { StatsPageService } from './stats-page.service';
+
+@Component({
+  selector: 'osm-stats-loader',
+  template: `<ng-container *ngComponentOutlet="component"></ng-container>`,
+  imports: [NgComponentOutlet],
+  providers: [StatsPageService],
+})
+export class StatsLoaderComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private statsPageSvc = inject(StatsPageService);
+  protected component = inject(StatsPageService).initComponent(this.route.snapshot.data);
+
+  ngOnInit() {
+    this.statsPageSvc.initPage().subscribe();
+  }
+}
---
+++ src/app/pages/stats/stats-page.service.ts
@@ -0,0 +1,91 @@
+import { finalize, forkJoin, tap } from 'rxjs';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { FixtureService } from '../../services/fixture.service';
+import { PlayerService } from '../../services/player.service';
+import { StatsService } from '../../services/stats.service';
+import { SeasonStats } from '../../models/season-stats.model';
+import { StatsPageComponent } from './d/stats.page';
+import { StatsMPageComponent } from './m/stats.m.page';
+import { Data } from '@angular/router';
+import { SeasonService } from '../../services/season.service';
+import { SelectOption } from '../../models/option.model';
+
+@Injectable()
+export class StatsPageService {
+  playerData = signal<any>([]);
+  headerTxt = signal('');
+
+  protected isMobile = signal(false);
+  protected isLoading = signal(true);
+  protected seasonStats = signal<SeasonStats | null>(null);
+
+  protected seasonPoints = computed(() => {
+    const wins = this.seasonStats()?.gamesWon ?? 0;
+    const draws = this.seasonStats()?.gamesDrawn ?? 0;
+    return wins * 3 + draws;
+  });
+
+  // protected isMobile = inject(IS_MOBILE);
+
+  private statsSvc = inject(StatsService);
+  private seasonSvc = inject(SeasonService);
+  private playerSvc = inject(PlayerService);
+  private fixtureSvc = inject(FixtureService);
+
+  initComponent(data: Data) {
+    this.isMobile.set(data['mobile']);
+    this.headerTxt.set(data['header']);
+    return this.isMobile() ? StatsMPageComponent : StatsPageComponent;
+  }
+
+  seasons = signal<SelectOption[]>([]);
+
+  initPage() {
+    this.seasonSvc
+      .fetch()
+      .pipe(
+        tap((seasons) => {
+          const allOption: SelectOption = {
+            value: 'all',
+            label: 'All Seasons',
+          };
+          const sortedOptions = seasons
+            .map(
+              (s) =>
+                ({
+                  value: s.id,
+                  label: s.name,
+                }) as SelectOption,
+            )
+            .sort((a, b) => b.label.localeCompare(a.label));
+          this.seasons.set([allOption, ...sortedOptions]);
+        }),
+      )
+      .subscribe();
+
+    return forkJoin([this.fixtureSvc.fetch(), this.playerSvc.fetch()]).pipe(
+      tap((res) => {
+        const seasonResults = res[0].filter((f) => f.competition?.id === 'tXLXUh6I1K9FyXaPpUmm');
+        this.seasonStats.set(this.statsSvc.generateSeasonStats(seasonResults));
+
+        const playerStats = this.statsSvc.generatePlayerStats(seasonResults);
+        this.playerData.set(
+          res[1]
+            .filter((p) => !p.isLegend && p.squadNumber)
+            .map((p) => ({
+              id: p.id,
+              name: p.fullName,
+              position: p.position,
+              squadNumber: p.squadNumber,
+              goals: playerStats.goals.get(p.id) ?? 0,
+              assists: playerStats.assists.get(p.id) ?? 0,
+              contributions: playerStats.contributions.get(p.id) ?? 0,
+            })),
+        );
+      }),
+      finalize(() => {
+        this.isLoading.set(false);
+      }),
+    );
+  }
+}
