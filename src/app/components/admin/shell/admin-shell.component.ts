c6e34297b376291b4fe51b3406b860e61a1cc3e3
Max Cobbett <maxcuentas@pm.me>
2025-07-17 21:44:38 +0800

    refactor admin shell

--- src/app/components/admin/shell/admin-shell.component.ts
+++ src/app/components/admin/shell/admin-shell.component.ts
@@ -5,47 +5,8 @@ import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-admin-shell',
   imports: [NzMenuModule, RouterModule],
-  styles: `
-    :host {
-      display: block;
-      height: 100%;
-    }
-
-    a {
-      position: fixed;
-      left: 50%;
-      transform: translateX(-50%);
-    }
-
-    img {
-      width: 100px;
-      margin-top: -28px;
-    }
-
-    ul {
-      background-color: var(--color-light);
-      border: none;
-      padding: 0 12px;
-    }
-
-    .osm-container {
-      height: calc(100% - var(--banner-height-desktop));
-    }
-  `,
-  template: `
-    <a routerLink="/">
-      <img src="assets/osm-transparent.png" alt="app-logo" />
-    </a>
-
-    <ul nz-menu nzMode="horizontal">
-      <li nz-menu-item nzMatchRouter routerLink="/admin/players" (click)="onNavigate('players')">Players</li>
-      <li nz-menu-item nzMatchRouter routerLink="/admin/fixtures" (click)="onNavigate('fixtures')">Fixtures</li>
-    </ul>
-
-    <div class="osm-container">
-      <router-outlet />
-    </div>
-  `,
+  styleUrl: './admin-shell.component.css',
+  templateUrl: './admin-shell.component.html',
 })
 export class AdminShellComponent {
   constructor(private _router: Router) {}
