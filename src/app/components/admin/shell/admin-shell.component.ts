cc593de03f1172fdf36dbcd0f749f92d2520cf10
Max Cobbett <maxcuentas@pm.me>
2025-07-19 21:57:54 +0800

    clean up

--- src/app/components/admin/shell/admin-shell.component.ts
+++ src/app/components/admin/shell/admin-shell.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-admin-shell',
+  selector: 'osm-admin-shell',
   imports: [NzMenuModule, RouterModule],
   styleUrl: './admin-shell.component.css',
   templateUrl: './admin-shell.component.html',
 })
-export class AdminShellComponent {
-  constructor(private _router: Router) {}
-
-  protected onNavigate(route: string) {
-    this._router.navigate([`/admin/${route}`]);
-  }
-}
+export class AdminShellComponent {}
